c6a722bc4a1bd8cac42160b9a2b893346e2f5031
lars3016 <lars.v.ommeren@gmail.com>
2017-03-21 16:23:46 +0100

    Profil model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :orders, dependent: :destroy
+  has_one :profile, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
